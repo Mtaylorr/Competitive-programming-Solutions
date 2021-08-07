dd17804693a629c4d3cc35306191ec184b7ce3cd
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2021-08-07 17:06:45 +0200

    Create Codeforces_847_D.cpp
    #Treap

---
+++ DS/Treap/Codeforces_847_D.cpp
@@ -0,0 +1,160 @@
+//https://codeforces.com/contest/847/problem/D
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define max(a, b) (a>b ? a : b)
+#define min(a, b) (a<b ? a : b)
+#define FAST ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
+
+
+const int N = int(3e6) + 3;
+const int MOD = int(1e9) + 7;
+
+
+enum DIR{L,R};
+
+struct node ;
+extern node *emp;
+struct node{
+    int v;
+    int p,sz, freq;
+    node * child[2];
+    node (){
+        v=0;
+        p=INT_MIN;
+        freq=sz=0;
+
+        child[L]=child[R]=this;
+    }
+    node (ll val, node *l=emp, node*r =emp){
+        p=rand();
+        v=val;
+        freq=1;
+        child[L]=l;
+        child[R]=r;
+        push_up();
+    }
+    void push_up(){
+        sz = (child[L]->sz) + (child[R]->sz) + freq;
+    }
+};
+
+node *emp = new node();
+
+void split(node * rt, const ll & val, node * & ls, node* & ge){
+    if(rt==emp){
+        ls=ge=emp;
+        return ;
+    }
+    if(val<rt->v){
+        split(rt->child[L], val,ls,rt->child[L]);
+        ge=rt;
+    }else{
+        split(rt->child[R], val, rt->child[R], ge);
+        ls = rt;
+    }
+    rt->push_up();
+}
+
+node* mrg(node * ls, node * ge){
+    if(ls==emp)return ge;
+    if(ge==emp)return ls;
+    if(ls->p > ge->p){
+        ls->child[R]=mrg(ls->child[R], ge);
+        ls->push_up();
+        return ls;
+    }
+    ge->child[L]=mrg(ls, ge->child[L]);
+    ge->push_up();
+    return ge;
+
+}
+
+void insert(node *&rt, node * nd){
+    if(rt==emp){
+        rt=nd;
+        return ;
+    }
+    if(rt->p < nd->p){
+        split(rt, nd->v, nd->child[L], nd->child[R]);
+        rt=nd;
+    }else if(nd->v < rt->v){
+        insert(rt->child[L], nd);
+    }else{
+        insert(rt->child[R], nd);
+    }
+    rt->push_up();
+}
+
+bool incremented(node *rt, const ll & val){
+    if(rt==emp)return 0;
+    bool ret=false;
+    if(rt->v==val){
+        rt->freq++;
+        ret =true;
+    }
+    else if(rt->v>val)
+        ret =  incremented(rt->child[L],val);
+    else
+        ret = incremented(rt->child[R],val);
+    if(ret)rt->push_up();
+    return ret;
+}
+
+
+
+void insert(node *& rt, const int & val){
+    if(!incremented(rt, val))
+        insert(rt, new node(val));
+}
+
+
+const int & getByIndex(node *rt, int ind){
+
+    if(ind<rt->child[L]->sz){
+        return getByIndex(rt->child[L],ind);
+    }
+    int sm = rt->child[L]->sz + rt->freq;
+    if(ind>=sm){
+        return getByIndex(rt->child[R],ind-sm);
+    }
+    return rt->v;
+}
+int lower_bound(node *rt, const ll & val){
+    if(rt==emp)return 0;
+    if(rt->v==val)return rt->child[L]->sz;
+    if(rt->v>val){
+        return lower_bound(rt->child[L],val);
+    }
+    return lower_bound(rt->child[R],val)+(rt->child[L]->sz) + (rt->freq) ;
+}
+
+
+int t;
+int n;
+int T;
+int main() {
+    int ans=0;
+    node *rt=emp;
+    cin >> n >> T;
+    for(int i=0;i<n;i++){
+        cin >> t;
+        if(t>=T)
+            continue;
+        t=max(0,t-(i+1));
+        insert(rt,t);
+        int d = lower_bound(rt, T-(i+1));
+        ans=max(ans, d);
+    }
+    cout << ans << endl;
+    return 0;
+}
