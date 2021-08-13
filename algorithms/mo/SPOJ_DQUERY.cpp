5cd83771d05d4dd39b28df3eaabda96adccfc8bf
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2021-08-13 22:39:31 +0200

    Update SPOJ_DQUERY.cpp

--- algorithms/mo/SPOJ_DQUERY.cpp
+++ algorithms/mo/SPOJ_DQUERY.cpp
@@ -1,99 +1,75 @@
 //https://www.spoj.com/problems/DQUERY/
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<ll> vl;
-typedef pair<ll, ll> pl;
-typedef long double ld;
- 
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(),(v).rend()
-#define max(a, b) (a>b ? a : b)
-#define min(a, b) (a<b ? a : b)
-#define FAST ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
- 
- 
-const int N = int(1e6) + 3;
-const int MOD = int(1e9) + 7;
- 
- 
- 
-int batch;
-int l[N],r[N];
-int n,q;
-vector<int> queries;
-int ans[N];
-int a[N];
-unordered_map<int,int> cnt;
-bool cmp(int x, int y){
-    if(l[x]/batch!=l[y]/batch){
-        return l[x]<l[y];
-    }
-    return r[x]<r[y];
+
+const int N = int(1e6)+5;
+
+int n, a[N], q;
+int  qs[N], qe[N], qi[N], s, e, qa[N];
+int cnt;
+int freq[N];
+
+void add(int val){
+    if(freq[val]==0)
+        cnt++;
+    freq[val]++;
 }
-int nb=0;
- 
-void add(int x){
-    cnt[x]++;
-    if(cnt[x]==1){
-        nb++;
+
+void rem(int val){
+    freq[val]--;
+    if(freq[val]==0)
+        cnt--;
+}
+
+void update(int cur){
+    while(e<qe[cur]){
+        add(a[++e]);
+    }
+    while(s>qs[cur]){
+        add(a[--s]);
+    }
+    while(e>qe[cur]){
+        rem(a[e--]);
+    }
+    while(s<qs[cur]){
+        rem(a[s++]);
     }
 }
- 
-void rem(int x){
-    cnt[x]--;
-    if(cnt[x]==0){
-        nb--;
+
+void mo(){
+    int sq = sqrt(n);
+    sort(qi, qi+q, [sq](int a, int b){return make_pair(qs[a]/sq,qe[a])<make_pair(qs[b]/sq, qe[b]);});
+    s=0;
+    e=-1;
+    cnt=0;
+    for(int i=0;i<q;i++){
+        int cur = qi[i];
+        update(cur);
+        qa[cur]=cnt;
     }
 }
- 
-int main() {
-    FAST;
-    cin >> n;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    scanf("%d",&n);
     for(int i=0;i<n;i++){
-        cin >> a[i];
+        scanf("%d",&a[i]);
     }
-    batch = sqrt(n);
-    cin >> q;
+    scanf("%d",&q);
     for(int i=0;i<q;i++){
-        queries.pb(i);
-        cin >> l[i] >> r[i];
-        l[i]--;
-        r[i]--;
+        qi[i]=i;
+        scanf("%d%d",&qs[i], &qe[i]);
+        qs[i]--;
+        qe[i]--;
     }
-    sort(all(queries),cmp);
-    int curl=0;
-    int curr=-1;
-    for(auto i:queries){
-        while(curr<r[i]){
-            curr++;
-            add(a[curr]);
-        }
-        while(curl>l[i]){
-            curl--;
-            add(a[curl]);
-        }
-        while(curr>r[i]){
-            rem(a[curr]);
-            curr--;
-        }
-        while(curl<l[i]){
-            rem(a[curl]);
-            curl++;
-        }
-        ans[i] = nb;
-    }
+    mo();
     for(int i=0;i<q;i++){
-        cout << ans[i] << "\n";
+        printf("%d\n",qa[i]);
     }
- 
- 
+
+
     return 0;
-} 
+}
+
