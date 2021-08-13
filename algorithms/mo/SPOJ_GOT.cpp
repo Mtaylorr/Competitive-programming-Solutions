85baecbf07e270435c5828af9671bdc6e4b0b59d
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2021-08-13 23:40:22 +0200

    Update SPOJ_GOT.cpp

--- algorithms/mo/SPOJ_GOT.cpp
+++ algorithms/mo/SPOJ_GOT.cpp
@@ -1,181 +1,171 @@
 //https://www.spoj.com/problems/GOT/
 #include <bits/stdc++.h>
- 
+
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<ll> vl ;
-typedef pair<ll,ll> pl;
- 
-#define mp make_pair
+
 #define pb push_back
-#define fi first
-#define se second
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(),(v).rend()
-#define max(a,b) (a>b ? a : b)
-#define min(a,b) (a<b ? a : b)
-#define FAST ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
- 
-const int MOD = (int)7+(int)1e9 ;
-const int N = 1000005;
-const double PI =4*atan(1);
-const double eps = 1e-10;
-const long long oo = 1e10;
-const int K = 26;
- 
- 
-int qe[N],qs[N],qi[N],qv[N],val[N],qlca[N];
-int n,m;
-int ans[N];
-int l,r;
-int st[N],et[N];
-vector<int> a;
-int occ[N],freq[N];
- 
-void change(int cur){
-    occ[cur]^=1;
-    
-    if(occ[cur]){
-        freq[val[cur]]++;
-    }else{
-        freq[val[cur]]--;
-    }
-    //cout << cur << " " << val[cur] << " " << freq[val[cur]] << endl;
-}
- 
-void update(int cur){
-    while(r<qe[cur])
-        change(a[++r]);
-    while(l>=qs[cur])
-        change(a[--l]);
-    while(r>qe[cur])
-        change(a[r--]);
-    while(l<qs[cur])
-        change(a[l++]);
-}
- 
-void mo()
-{
-    int bs = sqrt(2*n);
-    sort(qi,qi+m,[bs](int a, int b){return mp(qs[a]/bs,qe[a])<mp(qs[b]/bs,qe[b]);});
-    l=0;
-    r=-1;
-    for(int i=0;i<m;i++){
-        int cur = qi[i];
-        freq[val[qlca[cur]]]++;
-        update(cur);
-    	//cout << freq[qv[cur]] << endl;
-        ans[cur]=(freq[qv[cur]]>0);
-        freq[val[qlca[cur]]]--;
-    }
-}
+
+const int N = int(3e5)+5;
+
+int n, a[N], q;
+int  qs[N], qe[N], qi[N], s, e, qa[N], qlca[N],qk[N];
+bool occ[N];
+int freq[N];
+
 vector<vector<int> > adj;
-vector<int> depth, node2ind,ind2node;
- 
- 
-void init()
-{
-    for(int i=0;i<=n;i++){
-        occ[i]=freq[i]=0;
-    }
+vector<int> node2ind, ind2node, depth;
+int spt[int(5e5)+5][20];
+int lg[int(5e5)+5];
+
+vector<int> st , et, dfsorder;
+
+
+void init(){
     adj.clear();
     adj.resize(n+1);
     depth.resize(n+1);
     node2ind.resize(n+1);
     ind2node.clear();
+    st.resize(n+1);
+    et.resize(n+1);
+    dfsorder.clear();
+    memset(freq, 0, sizeof(freq));
+    memset(occ, 0, sizeof(occ));
 }
-void dfs(int u,int dpth=0,int p=0)
-{
+
+void dfs(int u, int dpth=0, int p=0){
     node2ind[u]=ind2node.size();
-    ind2node.push_back(u);
+    ind2node.pb(u);
     depth[u]=dpth;
-    st[u]=a.size();
-    a.pb(u);
-    for(int v:adj[u])
-    {
+    st[u]=dfsorder.size();
+    dfsorder.pb(u);
+    for(int v:adj[u]){
         if(v==p)
             continue;
-        dfs(v,dpth+1,u);
-        ind2node.push_back(u);
+        dfs(v, dpth+1, u);
+        ind2node.pb(u);
     }
-    et[u]=(a.size());
-    a.pb(u);
+    et[u]=dfsorder.size();
+    dfsorder.pb(u);
 }
-int lgs[int(2e5)+5];
-int spt[int(2e5)+5][20];
-void buildSpt()
-{
-    lgs[0]=-1;
-    for(int i=0;i<ind2node.size();i++)
-    {
+
+void buildspt(){
+    lg[0]=-1;
+    for(int i=0;i<ind2node.size();i++){
         spt[i][0]=i;
-        lgs[i+1]=lgs[i]+!(i&(i+1));
+        lg[i+1]= lg[i]+ !(i&i+1);
     }
-    for(int j=1;(1<<j)<=ind2node.size();j++)
-    {
-        for(int i=0;i+(1<<j)<=ind2node.size();i++)
-        {
-            int a=spt[i][j-1];
-            int b=spt[i+(1<<(j-1))][j-1];
-            spt[i][j]=(depth[ind2node[a]]<depth[ind2node[b]])?a:b;
+    for(int j=1;(1<<j)<=ind2node.size();j++){
+        for(int i=0;i+(1<<j)<=ind2node.size();i++){
+            int a = spt[i][j-1];
+            int b = spt[i+(1<<(j-1))][j-1];
+            spt[i][j] = (depth[ind2node[a]]<depth[ind2node[b]] ? a:b);
         }
     }
 }
- 
-int SPTRMQ(int a,int b)
-{
-    int sz=b-a+1;
-    int lg=lgs[sz];
-    a=spt[a][lg];
-    b=spt[b-(1<<lg)+1][lg];
-    return (depth[ind2node[a]]<depth[ind2node[b]])?a:b;
+
+int SPTMQ(int a , int b){
+    int sz = b-a+1;
+    int l = lg[sz];
+    a = spt[a][l];
+    b = spt[b-(1<<l)+1][l];
+    return (depth[ind2node[a]]<depth[ind2node[b]] ? a:b);
 }
-int SPTLCA(int u,int v)
-{
-    int a=node2ind[u];
-    int b=node2ind[v];
+
+int SPTLCA(int u , int v){
+    int a = node2ind[u];
+    int b = node2ind[v];
     if(a>b)
         swap(a,b);
-    return ind2node[SPTRMQ(a,b)];
+    return ind2node[SPTMQ(a,b)];
 }
-int u,v,w;
-int main(){
-    while(cin>> n){
-        init();
-        scanf("%d",&m);
-        for(int i=1;i<=n;i++){
-            scanf("%d",val+i);
-        }
-        for(int i=0;i<n-1;i++){
-            scanf("%d %d",&u,&v);
-            adj[u].pb(v);
-            adj[v].pb(u);
-        }
-        dfs(1);
-        buildSpt();
-        for(int i=0;i<m;i++){
-            scanf("%d%d%d",&u,&v,&w);
-            qi[i]=i;
-            if(st[u]>st[v])swap(u,v);
-            qlca[i]=SPTLCA(u,v);
-            if(qlca[i]==u){
-                qs[i]=st[u];
-                qe[i]=st[v];
-            }else{
-                qs[i]=et[u];
-                qe[i]=st[v];
-            }
-            qv[i]=w;
-        }
-       
-        mo();
-        for(int i=0;i<m;i++){
-            if(ans[i])printf("Find\n");
-            else printf("NotFind\n");
-        }
+
+
+
+void add(int val){
+    freq[val]++;
+}
+
+
+void rem(int val){
+    freq[val]--;
+
+}
+
+void change(int val){
+    if(occ[val]^=1)
+        add(a[val]);
+    else{
+        rem(a[val]);
     }
- 
- 
+}
+void update(int cur){
+    while(e<qe[cur]){
+        change(dfsorder[++e]);
+    }
+    while(s>qs[cur]){
+        change(dfsorder[--s]);
+    }
+    while(e>qe[cur]){
+        change(dfsorder[e--]);
+    }
+    while(s<qs[cur]){
+        change(dfsorder[s++]);
+    }
+}
+
+void mo(){
+    int sq = sqrt(dfsorder.size());
+    sort(qi, qi+q, [sq](int a, int b){return make_pair(qs[a]/sq,qe[a])<make_pair(qs[b]/sq, qe[b]);});
+    s=0;
+    e=-1;
+    for(int i=0;i<q;i++){
+        int cur = qi[i];
+        update(cur);
+        add(a[qlca[cur]]);
+        qa[cur]=freq[qk[cur]]!=0;
+        rem(a[qlca[cur]]);
+    }
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    while(~scanf("%d%d",&n,&q)){
+        for(int i=0;i<n;i++){
+        cin >> a[i+1];
+    }
+    int u,v, k;
+    init();
+    for(int i=1;i<n;i++){
+
+        scanf("%d%d",&u,&v);
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    dfs(1);
+    buildspt();
+    for(int i=0;i<q;i++){
+        scanf("%d%d%d",&u,&v,&k);
+        qk[i]=k;
+        qi[i]=i;
+        if(st[u]>st[v])
+            swap(u,v);
+        int lca = SPTLCA(u,v);
+        qlca[i]=lca;
+        if(lca==u){
+            qs[i] = st[u]+1;
+            qe[i] = st[v];
+        }else{
+            qs[i] = et[u];
+            qe[i] = st[v];
+        }
+
+    }
+    mo();
+    for(int i=0;i<q;i++){
+        printf("%s\n", (qa[i]?"Find":"NotFind"));
+    }
+}
     return 0;
-} 
+}
