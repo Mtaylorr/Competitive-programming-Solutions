7085d1be4b0f420b5f7334e8171023efbb74ceb4
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2021-10-20 09:18:43 +0200

    Create ProblemA.cpp

---
+++ TCPC/2021/ProblemA.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define fi first
+#define se second
+
+typedef long long ll;
+
+using namespace std;
+
+
+const int N = 1e5+5;
+const int MOD = 1e9 +7;
+
+int t,n,k;
+int main(){
+    freopen("input.txt", "r", stdin);
+    //freopen("rabbits.in", "r", stdin);
+    scanf("%d%d",&n,&k);
+    printf("%d",n*(k+1));
+    return 0;
+}
