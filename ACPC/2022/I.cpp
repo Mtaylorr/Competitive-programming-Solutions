ae2a4b98beda9d4416ee53ac41984a185590fea2
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2023-01-24 21:38:19 +0100

    Create I.cpp

---
+++ ACPC/2022/I.cpp
@@ -0,0 +1,44 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+int x;
+void test_case() {
+    cin >> x;
+    if (x == 2023 || x == 2022) {
+        cout << "Egypt\n";
+    } else {
+        cout << "Elsewhere\n";
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    // cin >>T;
+    T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
