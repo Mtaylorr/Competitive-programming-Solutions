d956c263e605c86f901360f6edcb7ce6d6c9d10b
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2023-01-24 21:54:10 +0100

    Create L.cpp

---
+++ ACPC/2022/L.cpp
@@ -0,0 +1,54 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 1e6 + 5;
+
+int n, m;
+int ok[N];
+
+void test_case() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int x,y;
+        cin >> x >> y;
+        if (y == x + 1) {
+            ok[x] = 1;
+        }
+    }
+    ok[n]=1;
+    for(int i=1;i<=n;i++){
+        cout << 1-ok[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    // cin >>T;
+    T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
