23cde3945010e4a5cc76728d49171c557c9afea3
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-05-10 20:26:00 +0200

    Create problemK.cpp

---
+++ ACPC/Kickoff/2023/problemK.cpp
@@ -0,0 +1,85 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+const int MOD = 1e9 + 7;
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+template <typename T>
+int powe(T u, ll v, int mod = MOD) {
+    int ans = 1;
+    int acc = u % mod;
+    while (v) {
+        if (v % 2) ans = mul(ans, acc, mod);
+        acc = mul(acc, acc, mod);
+        v /= 2;
+    }
+    return ans;
+}
+template <typename T>
+int inv(T u, int mod = MOD) {
+    return powe(u, mod - 2, mod);
+}
+int inv6 = inv(6);
+int inv2 = inv(2);
+ll squaresum(ll x) { return mul(mul(x, x + 1), mul(2 * x + 1, inv6)); }
+ll evensqauresum(ll x) { return mul(4, squaresum(x / 2)); }
+ll oddsquaresum(ll x) { return sub(squaresum(x), evensqauresum(x - 1)); }
+ll sum(ll x) { return mul(mul(x, x + 1), inv2); }
+ll evensum(ll x) { return mul(2, sum(x / 2)); }
+ll oddsum(ll x) { return sub(sum(x), evensum(x - 1)); }
+
+int n;
+void test_case() {
+    cin >> n;
+    if (n % 2 == 0) {
+        ll ans = mul(2, oddsquaresum(n - 1));
+        ans = add(ans, oddsum(n-1));
+        cout << ans << endl;
+    }else{
+        ll ans = mul(2, evensqauresum(n - 1));
+        ans = add(ans, evensum(n-1));
+        cout << ans << endl;
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
