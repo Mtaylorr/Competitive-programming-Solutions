3991fc13ccdbe2ef03dea2af07f1799bd9960080
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-05-10 20:08:17 +0200

    Create problemI.cpp

---
+++ ACPC/Kickoff/2023/problemI.cpp
@@ -0,0 +1,56 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+map<int, int> freq;
+int n;
+vector<int> tmp;
+int a, k;
+void test_case() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        freq[a]++;
+    }
+    for (auto u : freq) {
+        tmp.pb(u.se);
+    }
+    sort(all(tmp));
+    int nb = tmp.size();
+    for (auto u : tmp) {
+        if (u > k) break;
+        k -= u;
+        nb--;
+    }
+    cout << nb << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
