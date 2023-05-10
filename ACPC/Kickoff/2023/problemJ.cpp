d817d29a7adb8065e6f65f260d3eb68085238535
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-05-10 20:13:20 +0200

    Create problemJ.cpp

---
+++ ACPC/Kickoff/2023/problemJ.cpp
@@ -0,0 +1,56 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+map<int, int> dp;
+int a[N];
+int n, q, x, k;
+
+void test_case() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        dp[i] = max(dp[a[i + 1]], a[i + 1] - a[i]);
+    }
+    cin >> q;
+    while (q--) {
+        cin >> x >> k;
+        if (dp[x] <= k) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
