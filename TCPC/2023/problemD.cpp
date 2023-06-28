c22eb525dbb511cb95b9dcd34b3b8de64a41694a
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-06-28 22:02:07 +0200

    Create problemD.cpp

---
+++ TCPC/2023/problemD.cpp
@@ -0,0 +1,81 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 1e6 + 5;
+const int MOD = 1e9 + 7;
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+template <typename T>
+int powe(T u, ll v, int mod = MOD) {
+    int ans = 1;
+    int acc = u % mod;
+    while (v) {
+        if (v % 2) ans = mul(ans, acc, mod);
+        acc = mul(acc, acc, mod);
+        v /= 2;
+    }
+    return ans;
+}
+template <typename T>
+int inv(T u, int mod = MOD) {
+    return powe(u, mod - 2, mod);
+}
+
+int dp[N];
+int n;
+void preprocess() {
+    dp[1] = 0;
+    int f = 1;
+    for (int i = 2; i <= 1e6; i++) {
+        f = (f * 1ll * i) % MOD;
+        dp[i] = (dp[i - 1] + 0ll + MOD - ((i - 1) * 1ll * dp[i - 1]) % MOD +
+                 (f * (i - 1)) % MOD) %
+                MOD;
+    }
+}
+
+void test_case() {
+    cin >> n;
+    cout << dp[n] << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    preprocess();
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
