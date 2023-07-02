fe37c1a7196423cd15b77c05e6ea28b8b70e17b7
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-07-02 23:53:19 +0200

    Create F.cpp

---
+++ Gym/104447/F.cpp
@@ -0,0 +1,218 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+#define left(x) (2 * (x) + 1)
+#define right(x) (2 * (x) + 2)
+#define mid(ns, ne) ((ns) + ((ne) - (ns)) / 2)
+
+class segtree {
+   public:
+    struct node {
+        // define the variables
+        int add = 0;
+        ll mn = 0;
+        node() {
+            // define initial values
+            mn = 0;
+        }
+        void apply(int ns, int ne, ll v) {
+            // define apply operation
+            mn = v;
+        }
+    };
+
+    node unite(const node &a, const node &b) const {
+        node res;
+        // define the union operation
+        res.mn = min(a.mn, b.mn);
+        return res;
+    }
+
+    inline void push(int id, int ns, int ne) {
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        if (tree[id].add != 0) {
+            tree[l].apply(ns, md, tree[id].add);
+            tree[r].apply(md + 1, ne, tree[id].add);
+            tree[id].add = 0;
+        }
+    }
+
+    inline void pull(int id, int l, int r) {
+        tree[id] = unite(tree[l], tree[r]);
+    }
+    int n;
+    vector<node> tree;
+
+    void build(int id, int ns, int ne) {
+        if (ns == ne) {
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        build(l, ns, md);
+        build(r, md + 1, ne);
+        pull(id, l, r);
+    }
+    template <typename M>
+    void build(int id, int ns, int ne, const vector<M> &v) {
+        if (ns == ne) {
+            tree[id].apply(ns, ne, v[ns]);
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        build(l, ns, md, v);
+        build(r, md + 1, ne, v);
+        pull(id, l, r);
+    }
+    node get(int qs, int qe, int id, int ns, int ne) {
+        if (qs <= ns && qe >= ne) {
+            return tree[id];
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        push(id, ns, ne);
+        node res{};
+        if (qe <= md) {
+            res = get(qs, qe, l, ns, md);
+        } else {
+            if (qs > md) {
+                res = get(qs, qe, r, md + 1, ne);
+            } else {
+                res = unite(get(qs, qe, l, ns, md), get(qs, qe, r, md + 1, ne));
+            }
+        }
+        pull(id, l, r);
+        return res;
+    }
+
+    template <typename... M>
+    void modify(int qs, int qe, int id, int ns, int ne, const M &...v) {
+        if (qs <= ns && qe >= ne) {
+            tree[id].apply(ns, ne, v...);
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        push(id, ns, ne);
+        if (qs <= md) {
+            modify(qs, qe, l, ns, md, v...);
+        }
+        if (qe > md) {
+            modify(qs, qe, r, md + 1, ne, v...);
+        }
+        pull(id, l, r);
+    }
+
+    segtree(int _n) : n(_n) {
+        assert(n > 0);
+        tree.resize(4 * n);
+        build(0, 0, n - 1);
+    }
+
+    template <typename M>
+    segtree(const vector<M> &v) {
+        n = v.size();
+        assert(n > 0);
+        tree.resize(4 * n);
+        build(0, 0, n - 1, v);
+    }
+
+    node get(int qs, int qe) {
+        assert(0 <= qs && qs <= qe && qe <= n - 1);
+        return get(qs, qe, 0, 0, n - 1);
+    }
+    node get(int p) {
+        assert(0 <= p && p <= n - 1);
+        return get(p, p, 0, 0, n - 1);
+    }
+
+    template <typename... M>
+    void upd(int qs, int qe, const M &...v) {
+        assert(0 <= qs && qs <= qe && qe <= n - 1);
+        modify(qs, qe, 0, 0, n - 1, v...);
+    }
+};
+
+ll s[N];
+int n, k;
+string w[N];
+int sum[N];
+
+void test_case() {
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i];
+        sum[i] = 0;
+    }
+    segtree sg = segtree(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> s[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        int m = w[i].size();
+        if (i > 0) sum[i] += sum[i - 1];
+        for (int j = 0; j < m / 2; j++) {
+            sum[i] += w[i][j] != w[i][m - j - 1];
+        }
+    }
+    ll ans = 0;
+    ll csum = 0;
+    for (int i = 1; i <= n; i++) {
+        csum += s[i];
+        sg.upd(i, i, csum);
+        int l = 1;
+        int r = i;
+        while (l < r) {
+            int md = (l + r) / 2;
+            if (sum[i] - sum[md - 1] <= k) {
+                r = md;
+            } else {
+                l = md + 1;
+            }
+        }
+        if (sum[i] - sum[l - 1] <= k) {
+            //dbg(l,i,sum[i]-sum[l-1]);
+            ans = max(ans, csum - sg.get(l-1, i).mn);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
