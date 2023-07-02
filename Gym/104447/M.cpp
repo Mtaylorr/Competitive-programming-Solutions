f0838789087a8ad5f98ff692398850c0877c6178
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-07-02 23:51:24 +0200

    Create M.cpp

---
+++ Gym/104447/M.cpp
@@ -0,0 +1,49 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+ll n, m;
+
+void test_case() {
+    cin >> n >> m;
+    if (n == 0 && m == 0) {
+        cout << 0 << endl;
+    } else if (n == m) {
+        cout << 1 << " " << n << endl;
+    } else if (abs(n) % 2 != abs(m) % 2) {
+        cout << -1 << endl;
+    } else {
+        cout << 2 << " " << (n + m) / 2 << " " << (n - m) / 2 << endl;
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
