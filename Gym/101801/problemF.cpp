54defff0881980b9c876cae5d0b3dc6410b48fe8
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-07-09 23:14:33 +0200

    Create problemF.cpp

---
+++ Gym/101801/problemF.cpp
@@ -0,0 +1,50 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+int n;
+int s[N];
+void test_case() {
+    cin >> n;
+    int x=0;
+    for(int i=0;i<n;i++){
+        cin >> s[i];
+        x=x^s[i];
+    }
+    if(x!=0){
+        cout << 0 << endl;
+    }else{
+        cout << 1 << endl;
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
