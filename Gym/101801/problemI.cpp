e99957805803711e7bca793209c06fa9d2d930c7
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2023-07-09 23:16:59 +0200

    Create problemI.cpp

---
+++ Gym/101801/problemI.cpp
@@ -0,0 +1,92 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+const int MOD = 1e9 + 7;
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+template <typename T>
+int powe(T u, ll v, int mod = MOD) {
+    int ans = 1;
+    int acc = u % mod;
+    while (v) {
+        if (v % 2) ans = mul(ans, acc, mod);
+        acc = mul(acc, acc, mod);
+        v /= 2;
+    }
+    return ans;
+}
+template <typename T>
+int inv(T u, int mod = MOD) {
+    return powe(u, mod - 2, mod);
+}
+
+int n, q;
+int a[N];
+int sum[2][N];
+
+void test_case() {
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        sum[0][i] = sum[0][i - 1];
+        sum[1][i] = sum[1][i - 1];
+        sum[a[i] % 2][i]++;
+    }
+    while (q--) {
+        int ty, l, r;
+        cin >> l >> r >> ty;
+        if (ty == 0) {
+            int sm1 = sum[1][r] - sum[1][l - 1];
+            int sm0 = sum[0][r] - sum[0][l - 1];
+            int ans = (sub(powe(2, sm0), 1) * 1ll * powe(2, sm1)) % MOD;
+            cout << ans << endl;
+        } else {
+            int sm = sum[1][r] - sum[1][l - 1];
+            cout << sub(powe(2, sm), 1) << endl;
+        }
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
