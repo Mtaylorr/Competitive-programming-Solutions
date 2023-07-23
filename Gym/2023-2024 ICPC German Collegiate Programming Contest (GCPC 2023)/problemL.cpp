f94c77dd043cc5e554a339255a141b9972db7a76
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-07-23 23:29:23 +0200

    Create problemL.cpp

---
+++ Gym/2023-2024 ICPC German Collegiate Programming Contest (GCPC 2023)/problemL.cpp
@@ -0,0 +1,69 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+string s;
+string t;
+int n;
+void test_case() {
+    cin >> s;
+    n = s.size();
+    int nb = 0;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (cnt > 5) {
+            cout << "no\n";
+            return;
+        }
+        if (s[i] == '(') {
+            nb++;
+        } else {
+            nb--;
+            t="";
+            if (nb == 0 && i < n - 1) {
+                for (int j = i + 1; j < n; j++) {
+                    t += s[j];
+                }
+                for (int j = 0; j <= i; j++) {
+                    t += s[j];
+                }
+                if(t!=s){
+                    cout << t << endl;
+                    return ;
+                }
+                cnt++;
+            }
+        }
+    }
+    cout << "no\n";
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
