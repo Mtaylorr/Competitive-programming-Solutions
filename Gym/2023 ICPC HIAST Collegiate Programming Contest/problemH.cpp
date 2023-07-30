6107fd84b4e98b748cded0913c39b99c12ae3088
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-07-30 23:44:49 +0200

    Create problemH.cpp

---
+++ Gym/2023 ICPC HIAST Collegiate Programming Contest/problemH.cpp
@@ -0,0 +1,235 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+struct DSUArray {
+    vector<int> id, l, r, sz;
+    void init(int n) {
+        id.assign(n, 0);
+        l.assign(n, 0);
+        r.assign(n, 0);
+        sz.assign(n, 0);
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            l[i] = i;
+            r[i] = i;
+            sz[i] = 1;
+        }
+    }
+    int getid(int u) { return (u == id[u] ? u : id[u] = getid(id[u])); }
+    bool sameSet(int u, int v) { return getid(u) == getid(v); }
+    void uni(int u, int v) {
+        v = getid(v);
+        u = getid(u);
+        if (u == v) return;
+        id[u] = v;
+        l[v] = min(l[v], l[u]);
+        r[v] = max(r[v], r[u]);
+        sz[v] += sz[u];
+    }
+} dsu;
+
+#define left(x) (2 * (x) + 1)
+#define right(x) (2 * (x) + 2)
+#define mid(ns, ne) ((ns) + ((ne) - (ns)) / 2)
+
+class segtree {
+   public:
+    struct node {
+        // define the variables
+        int add = 0;
+        ll mx = 0;
+        node() {
+            // define initial values
+            mx = 0;
+        }
+        void apply(int ns, int ne, ll v) {
+            // define apply operation
+            mx += v;
+        }
+    };
+
+    node unite(const node &a, const node &b) const {
+        node res;
+        // define the union operation
+        res.mx = max(a.mx, b.mx);
+        return res;
+    }
+
+    inline void push(int id, int ns, int ne) {
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        if (tree[id].add != 0) {
+            tree[l].apply(ns, md, tree[id].add);
+            tree[r].apply(md + 1, ne, tree[id].add);
+            tree[id].add = 0;
+        }
+    }
+
+    inline void pull(int id, int l, int r) {
+        tree[id] = unite(tree[l], tree[r]);
+    }
+    int n;
+    vector<node> tree;
+
+    void build(int id, int ns, int ne) {
+        if (ns == ne) {
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        build(l, ns, md);
+        build(r, md + 1, ne);
+        pull(id, l, r);
+    }
+    template <typename M>
+    void build(int id, int ns, int ne, const vector<M> &v) {
+        if (ns == ne) {
+            tree[id].apply(ns, ne, v[ns]);
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        build(l, ns, md, v);
+        build(r, md + 1, ne, v);
+        pull(id, l, r);
+    }
+    node get(int qs, int qe, int id, int ns, int ne) {
+        if (qs <= ns && qe >= ne) {
+            return tree[id];
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        push(id, ns, ne);
+        node res{};
+        if (qe <= md) {
+            res = get(qs, qe, l, ns, md);
+        } else {
+            if (qs > md) {
+                res = get(qs, qe, r, md + 1, ne);
+            } else {
+                res = unite(get(qs, qe, l, ns, md), get(qs, qe, r, md + 1, ne));
+            }
+        }
+        pull(id, l, r);
+        return res;
+    }
+
+    template <typename... M>
+    void modify(int qs, int qe, int id, int ns, int ne, const M &...v) {
+        if (qs <= ns && qe >= ne) {
+            tree[id].apply(ns, ne, v...);
+            return;
+        }
+        int l = left(id);
+        int r = right(id);
+        int md = mid(ns, ne);
+        push(id, ns, ne);
+        if (qs <= md) {
+            modify(qs, qe, l, ns, md, v...);
+        }
+        if (qe > md) {
+            modify(qs, qe, r, md + 1, ne, v...);
+        }
+        pull(id, l, r);
+    }
+
+    segtree(int _n) : n(_n) {
+        assert(n > 0);
+        tree.resize(4 * n);
+        build(0, 0, n - 1);
+    }
+
+    template <typename M>
+    segtree(const vector<M> &v) {
+        n = v.size();
+        assert(n > 0);
+        tree.resize(4 * n);
+        build(0, 0, n - 1, v);
+    }
+
+    node get(int qs, int qe) {
+        assert(0 <= qs && qs <= qe && qe <= n - 1);
+        return get(qs, qe, 0, 0, n - 1);
+    }
+    node get(int p) {
+        assert(0 <= p && p <= n - 1);
+        return get(p, p, 0, 0, n - 1);
+    }
+
+    template <typename... M>
+    void upd(int qs, int qe, const M &...v) {
+        assert(0 <= qs && qs <= qe && qe <= n - 1);
+        modify(qs, qe, 0, 0, n - 1, v...);
+    }
+};
+
+int n;
+vector<int> a;
+bool done[N];
+void test_case() {
+    cin >> n;
+    dsu.init(n);
+    a.clear();
+    for (int i = 0; i < n; i++) {
+        done[i] = 0;
+        int x;
+        cin >> x;
+        a.pb(x);
+    }
+    segtree sg = segtree(a);
+    cout << sg.get(0, n - 1).mx << endl;
+    int q;
+    cin >> q;
+    while (q--) {
+        int p;
+        cin >> p;
+        --p;
+        ll x = sg.get(p, p).mx;
+        done[p] = 1;
+        if (done[p + 1]) {
+            dsu.uni(p, p + 1);
+        }
+        if (p && done[p - 1]) {
+            dsu.uni(p - 1, p);
+        }
+        int r = dsu.r[dsu.getid(p)] + 1;
+        sg.upd(r, r, x);
+        cout << sg.get(0, n - 1).mx << endl;
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
