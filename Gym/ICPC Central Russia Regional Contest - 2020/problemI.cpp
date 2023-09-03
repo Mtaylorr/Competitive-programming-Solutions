6e187a3a8844d5f98f2765fddb5929a8d03d6241
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-09-04 00:09:15 +0200

    Create problemI.cpp

---
+++ Gym/ICPC Central Russia Regional Contest - 2020/problemI.cpp
@@ -0,0 +1,62 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+ll n;
+map<ll, int> freq;
+void test_case() {
+    cin >> n;
+    ll x = n;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            while (x % i == 0) {
+                freq[i]++;
+                x /= i;
+            }
+        }
+    }
+    if (x > 1) {
+        freq[x]++;
+    }
+    ll ans = 1;
+    for (auto &u : freq) {
+        int p = (u.se + 1) / 2;
+        while (p--) {
+            ans *= u.fi;
+        }
+    }
+
+    ans = (ans + n) * (ans + n);
+    ll p = (ans/n - n);
+    cout << p << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
