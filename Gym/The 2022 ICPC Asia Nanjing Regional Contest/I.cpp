825465d48894166b6d1f3739cb2c9a6be504b8ec
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-10-08 23:04:18 +0200

    Create I.cpp

---
+++ Gym/The 2022 ICPC Asia Nanjing Regional Contest/I.cpp
@@ -0,0 +1,52 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+int n;
+string s;
+int freq[30];
+
+void test_case() {
+    memset(freq, 0, sizeof(freq));
+    cin >> s;
+    n = s.size();
+    for (auto &c : s) {
+        freq[c - 'a']++;
+    }
+    int ans = n;
+    for (int i = 0; i < 26; i++) {
+        ans = min(ans, n - freq[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
