6d81ebcbe01924487d114c671ddc7d902dc7b7a8
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-11-05 21:46:22 +0100

    Create I.cpp

---
+++ Gym/2023-2024 ACM-ICPC Latin American Regional Programming Contest/I.cpp
@@ -0,0 +1,90 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+const int MOD = 1e9 + 7;
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+template <typename T>
+int powe(T u, ll v, int mod = MOD) {
+    int ans = 1;
+    int acc = u % mod;
+    while (v) {
+        if (v % 2) ans = mul(ans, acc, mod);
+        acc = mul(acc, acc, mod);
+        v /= 2;
+    }
+    return ans;
+}
+template <typename T>
+int inv(T u, int mod = MOD) {
+    return powe(u, mod - 2, mod);
+}
+
+string s;
+ll n;
+int freq[50];
+void test_case() {
+    cin >> s;
+    cin >> n;
+    ll ans = 0;
+    ll inversions = 0;
+    for (int i = 0; i < s.size(); i++) {
+        int id = s[i] - 'a';
+        for (int j = id + 1; j < 26; j++) {
+            inversions += freq[j];
+        }
+        freq[id]++;
+    }
+    ans = mul(inversions, n);
+    ll p = mul(n, n - 1);
+    p = mul(p, inv(2));
+    for (int i = 1; i < 26; i++) {
+        for (int j = 0; j < i; j++) {
+            ll x = freq[i];
+            ll y = freq[j];
+            ans = (ans + mul(x, mul(y, p))) % MOD;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
