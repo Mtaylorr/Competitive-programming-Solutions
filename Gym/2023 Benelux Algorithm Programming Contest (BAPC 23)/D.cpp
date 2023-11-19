3dc2e1745e991209d5f79eae7f080b12026f550b
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-11-19 22:13:07 +0100

    Create D.cpp

---
+++ Gym/2023 Benelux Algorithm Programming Contest (BAPC 23)/D.cpp
@@ -0,0 +1,57 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+int freq[10005][26];
+int n, m;
+string s;
+void test_case() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        for (int j = 0; j < m; j++) {
+            freq[j][s[j] - 'a']++;
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        int mx = 0;
+        int mxpos = 0;
+        for (int j = 0; j < 26; j++) {
+            if (freq[i][j] > mx) {
+                mx = freq[i][j];
+                mxpos = j;
+            }
+        }
+        cout << char('a' + mxpos);
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
