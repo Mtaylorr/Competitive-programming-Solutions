a43824116efa9a1f685d7c280fbcb0b8076d399a
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-12-04 08:01:31 +0100

    Create M.cpp

---
+++ Gym/2023 United Kingdom and Ireland Programming Contest (UKIEPC 2023)/M.cpp
@@ -0,0 +1,45 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+int a, b, c;
+void test_case() {
+    cin >> a >> b >> c;
+    int ans = 2 * a;
+    if (c >= 2) {
+        ans += 2 * b + 3;
+        c -= 2;
+    }
+    ans+=(3*(c/2));
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
