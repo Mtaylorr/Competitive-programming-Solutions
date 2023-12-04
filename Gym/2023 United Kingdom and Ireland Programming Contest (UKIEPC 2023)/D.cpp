394f2cc6f9c738b64b5c10d57633c3134b476513
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2023-12-04 07:59:59 +0100

    Create D.cpp

---
+++ Gym/2023 United Kingdom and Ireland Programming Contest (UKIEPC 2023)/D.cpp
@@ -0,0 +1,52 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 1e6 + 5;
+
+int n;
+ll f[N];
+void test_case() {
+    ll ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> f[i];
+    }
+    sort(f, f + n);
+    reverse(f, f + n);
+    int nb = n / 3;
+    int i = 1;
+    while (nb--) {
+        ans += f[i];
+        i += 2;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
