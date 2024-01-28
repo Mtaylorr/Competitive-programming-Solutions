5d7b5f1bfbf82271b7e39b74fba375ab251001c0
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2024-01-28 21:45:54 +0100

    Create M.cpp

---
+++ Gym/2019-2020 ICPC NERC (NEERC), North-Western Russia Regional Contest (Northern Subregionals)/M.cpp
@@ -0,0 +1,54 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+int n;
+int a[N];
+unordered_map<int,int> freq;
+void test_case() {
+    cin >> n;
+    freq.clear();
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        freq[a[i]]++;
+    }
+    ll ans = 0;
+    for (int j = 0; j < n; j++) {
+        freq[a[j]]--;
+        for (int i = j - 1; i >= 0; i--) {
+            ans += freq[2 * a[j] - a[i]];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
