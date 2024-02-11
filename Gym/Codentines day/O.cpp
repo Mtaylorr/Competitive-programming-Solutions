1eed81cfc3b002f25b3330fa1b064fce74bd24fb
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2024-02-11 22:44:07 +0100

    Create O.cpp

---
+++ Gym/Codentines day/O.cpp
@@ -0,0 +1,53 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 1e6 + 5;
+int n, a;
+map<int, int> freq;
+
+void test_case() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        freq[a]++;
+    }
+    int nb = 0;
+    for (auto &u : freq) {
+        if (u.se >= 2) break;
+        nb++;
+    }
+    if (nb % 2) {
+        cout << "WIN\n";
+    } else {
+        cout << "LOSS\n";
+    }
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
