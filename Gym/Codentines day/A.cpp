da53a0a9d50fa8e42e94638a27ece3b7fc37c99e
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2024-02-11 22:39:37 +0100

    Create A.cpp

---
+++ Gym/Codentines day/A.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+//mt19937 RNG(chrono::steady_clock::now().time_since_epoch().count());
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimize("O3,unroll-loops")
+
+
+#define ll long long
+#define ld long double
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define PI 3.1415926535897932384626433832795
+#define EULER 2.7182818284590452353602874713
+#define F first
+#define S second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define sz(x) (int((x).size()))
+
+const ll mod = 1e9 + 7;
+const ll INF = 1e18 ;
+const ll MIN = -1e18 ;
+const ll MAXII = 200005 ;
+
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));}
+
+
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+ll inv(ll N) {if (N == 1) return 1; return (mod - ((mod / N) * inv(mod % N)) % mod) % mod;}
+void dbg(vector<ll> v){for (auto x : v) cout << x << " "; cout << endl;}
+void dbgg(pair<ll, ll> p){cout << p.F << " " << p.S << endl;}
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+//ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
+void star() { cout<<"*************************************\n"; }
+
+
+
+
+void solve() {
+    int n; cin>>n;
+    int sum = 0, a;
+    for (int i=0; i<n; i++){
+        cin>>a; sum+=a;
+    }
+    if (sum==14) yes();
+    else no();
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+
+    int tc=1;
+    //cin >> tc;
+    while(tc--) {
+        solve();
+    }
+}
