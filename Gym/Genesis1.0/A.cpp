bf333c32c0bbf33a91bef9b6e4382b2704c5ed36
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2024-05-11 22:37:35 +0200

    Create A.cpp

---
+++ Gym/Genesis1.0/A.cpp
@@ -0,0 +1,62 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+int n;
+int a[105][105];
+void test_case() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int ans = n * n;
+    for (int k = 0; k < 2; k++) {
+        int rs = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    rs += a[i][j] == k;
+                } else {
+                    rs += a[i][j] != k;
+                }
+            }
+        }
+        ans=min(ans,rs);
+    }
+    cout << ans << endl;
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
