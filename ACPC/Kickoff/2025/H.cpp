5deb0433b774ae1072a64b524b3ee4b72ea7ab27
Mahdi Cheikh Rouhou <cheikhrouhoumahdi@gmail.com>
2025-05-19 13:52:56 +0200

    Create H.cpp

---
+++ ACPC/Kickoff/2025/H.cpp
@@ -0,0 +1,86 @@
+// Mtaylor
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define endl '\n';
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+typedef long long ll;
+const int N = 3e5 + 5;
+
+const int MOD = 1e9 + 7;
+template <typename T, typename U>
+int mul(T u, U v, int mod = MOD) {
+    return ((u % mod) * 1ll * (v % mod)) % mod;
+}
+template <typename T, typename U>
+int add(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll + (v % mod) + mod) % mod;
+}
+template <typename T, typename U>
+int sub(T u, U v, int mod = MOD) {
+    return ((u % mod) + 0ll - (v % mod) + mod) % mod;
+}
+template <typename T>
+int powe(T u, ll v, int mod = MOD) {
+    int ans = 1;
+    int acc = u % mod;
+    while (v) {
+        if (v % 2) ans = mul(ans, acc, mod);
+        acc = mul(acc, acc, mod);
+        v /= 2;
+    }
+    return ans;
+}
+template <typename T>
+int inv(T u, int mod = MOD) {
+    return powe(u, mod - 2, mod);
+}
+int n, a[N];
+
+void test_case() {
+    cin >> n;
+    ll ans = 0;
+    ll s = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        s += a[i];
+        s %= MOD;
+    }
+    // step = 0;
+    ans += s;
+    ans %= MOD;
+    for (int i = 1; i < n; i++) {
+        int d = __gcd(i, n);
+        ll x = n / d;
+        ans = (ans + (x * 1ll * s) % MOD) % MOD;
+    }
+    cout << (ans * 1ll * inv((n * 1ll * n) % MOD) % MOD) % MOD << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
